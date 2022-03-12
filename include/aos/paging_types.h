573add5fc1e523c105b6d9cbb2237427754e4bd9
Florian Buetler <flbuetle@ethz.ch>
2022-03-12 18:03:41 +0100

    c format

--- include/aos/paging_types.h
+++ include/aos/paging_types.h
@@ -17,22 +17,20 @@
 
 #include <aos/solution.h>
 
-#define VADDR_OFFSET ((lvaddr_t)512UL*1024*1024*1024) // 1GB
-#define VREGION_FLAGS_READ     0x01 // Reading allowed
-#define VREGION_FLAGS_WRITE    0x02 // Writing allowed
-#define VREGION_FLAGS_EXECUTE  0x04 // Execute allowed
-#define VREGION_FLAGS_NOCACHE  0x08 // Caching disabled
-#define VREGION_FLAGS_MPB      0x10 // Message passing buffer
-#define VREGION_FLAGS_GUARD    0x20 // Guard page
-#define VREGION_FLAGS_MASK     0x2f // Mask of all individual VREGION_FLAGS
+#define VADDR_OFFSET ((lvaddr_t)512UL * 1024 * 1024 * 1024)  // 1GB
+#define VREGION_FLAGS_READ 0x01                              // Reading allowed
+#define VREGION_FLAGS_WRITE 0x02                             // Writing allowed
+#define VREGION_FLAGS_EXECUTE 0x04                           // Execute allowed
+#define VREGION_FLAGS_NOCACHE 0x08                           // Caching disabled
+#define VREGION_FLAGS_MPB 0x10                               // Message passing buffer
+#define VREGION_FLAGS_GUARD 0x20                             // Guard page
+#define VREGION_FLAGS_MASK 0x2f  // Mask of all individual VREGION_FLAGS
 
-#define VREGION_FLAGS_READ_WRITE \
-    (VREGION_FLAGS_READ | VREGION_FLAGS_WRITE)
-#define VREGION_FLAGS_READ_EXECUTE \
-    (VREGION_FLAGS_READ | VREGION_FLAGS_EXECUTE)
-#define VREGION_FLAGS_READ_WRITE_NOCACHE \
+#define VREGION_FLAGS_READ_WRITE (VREGION_FLAGS_READ | VREGION_FLAGS_WRITE)
+#define VREGION_FLAGS_READ_EXECUTE (VREGION_FLAGS_READ | VREGION_FLAGS_EXECUTE)
+#define VREGION_FLAGS_READ_WRITE_NOCACHE                                                 \
     (VREGION_FLAGS_READ | VREGION_FLAGS_WRITE | VREGION_FLAGS_NOCACHE)
-#define VREGION_FLAGS_READ_WRITE_MPB \
+#define VREGION_FLAGS_READ_WRITE_MPB                                                     \
     (VREGION_FLAGS_READ | VREGION_FLAGS_WRITE | VREGION_FLAGS_MPB)
 
 typedef int paging_flags_t;
