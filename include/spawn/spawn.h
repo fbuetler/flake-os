38b5d602ab6c43d1707562d6b779d77c0a1616d4
Florian Buetler <flbuetle@ethz.ch>
2022-03-21 18:34:09 +0100

    c format

--- include/spawn/spawn.h
+++ include/spawn/spawn.h
@@ -19,32 +19,28 @@
 #include "aos/paging.h"
 
 
-
 struct spawninfo {
     // the next in the list of spawned domains
-    struct spawninfo *next; 
+    struct spawninfo *next;
 
     // Information about the binary
-    char * binary_name;     // Name of the binary
+    char *binary_name;  // Name of the binary
 
-    struct mem_region *module; // same name as in the book
+    struct mem_region *module;  // same name as in the book
 
     // TODO(M2): Add fields you need to store state
     //           when spawning a new dispatcher,
     //           e.g. references to the child's
     //           capabilities or paging state
-
 };
 
 // Start a child process using the multiboot command line. Fills in si.
-errval_t spawn_load_by_name(char *binary_name, struct spawninfo * si,
-                            domainid_t *pid);
+errval_t spawn_load_by_name(char *binary_name, struct spawninfo *si, domainid_t *pid);
 
 // Start a child with an explicit command line. Fills in si.
-errval_t spawn_load_argv(int argc, char *argv[], struct spawninfo *si,
-                         domainid_t *pid);
-errval_t allocator_fn(void *state, genvaddr_t base, size_t size, uint32_t flags, void **ret);
-
+errval_t spawn_load_argv(int argc, char *argv[], struct spawninfo *si, domainid_t *pid);
+errval_t allocator_fn(void *state, genvaddr_t base, size_t size, uint32_t flags,
+                      void **ret);
 
 
 #endif /* _INIT_SPAWN_H_ */
