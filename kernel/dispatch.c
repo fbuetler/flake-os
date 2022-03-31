556baba609b0d49e1a6f6aba1452e81ba31911b8
Florian Buetler <flbuetle@ethz.ch>
2022-03-31 17:46:52 +0200

    c format

--- kernel/dispatch.c
+++ kernel/dispatch.c
@@ -31,11 +31,11 @@
 #include <bitmacros.h>
 
 #if defined(__x86_64__) || defined(__i386__)
-#  include <arch/x86/apic.h>
+#    include <arch/x86/apic.h>
 #endif
 
 #if defined(__x86_64__) && !defined(__k1om__)
-#  include <vmkit.h>
+#    include <vmkit.h>
 #endif
 
 
@@ -53,11 +53,11 @@ uint64_t context_switch_counter = 0;
 struct dcb *dcb_current = NULL;
 
 #if CONFIG_TRACE && NETWORK_STACK_BENCHMARK
-#define TRACE_N_BM 1
-#endif // CONFIG_TRACE && NETWORK_STACK_BENCHMARK
+#    define TRACE_N_BM 1
+#endif  // CONFIG_TRACE && NETWORK_STACK_BENCHMARK
 
 
-void __attribute__ ((noreturn)) dispatch(struct dcb *dcb)
+void __attribute__((noreturn)) dispatch(struct dcb *dcb)
 {
     // XXX FIXME: Why is this null pointer check on the fast path ?
     // If we have nothing to do we should call something other than dispatch
@@ -68,10 +68,8 @@ void __attribute__ ((noreturn)) dispatch(struct dcb *dcb)
 
     // Don't context switch if we are current already
     if (dcb_current != dcb) {
-
 #ifdef TRACE_CSWITCH
-        trace_event(TRACE_SUBSYS_KERNEL,
-                    TRACE_EVENT_KERNEL_CSWITCH,
+        trace_event(TRACE_SUBSYS_KERNEL, TRACE_EVENT_KERNEL_CSWITCH,
                     (uint32_t)(lvaddr_t)dcb & 0xFFFFFFFF);
 #endif
 
@@ -82,10 +80,8 @@ void __attribute__ ((noreturn)) dispatch(struct dcb *dcb)
     assert(dcb != NULL);
 
     dispatcher_handle_t handle = dcb->disp;
-    struct dispatcher_shared_generic *disp =
-        get_dispatcher_shared_generic(handle);
-    arch_registers_state_t *disabled_area =
-        dispatcher_get_disabled_save_area(handle);
+    struct dispatcher_shared_generic *disp = get_dispatcher_shared_generic(handle);
+    arch_registers_state_t *disabled_area = dispatcher_get_disabled_save_area(handle);
 
     if (disp != NULL) {
         disp->systime = systime_now() + kcb_current->kernel_off;
@@ -94,15 +90,13 @@ void __attribute__ ((noreturn)) dispatch(struct dcb *dcb)
 
     if (dcb->disabled) {
         if (disp != NULL) {
-            debug(SUBSYS_DISPATCH, "resume %.*s at 0x%" PRIx64 ", %s\n",
-		  DISP_NAME_LEN, disp->name,
-		  (uint64_t)registers_get_ip(disabled_area),
-		  disp->disabled ? "disp->disabled" : "disp->enabled"
-		);
+            debug(SUBSYS_DISPATCH, "resume %.*s at 0x%" PRIx64 ", %s\n", DISP_NAME_LEN,
+                  disp->name, (uint64_t)registers_get_ip(disabled_area),
+                  disp->disabled ? "disp->disabled" : "disp->enabled");
         }
 
 #if defined(__x86_64__) && !defined(__k1om__)
-        if(!dcb->is_vm_guest) {
+        if (!dcb->is_vm_guest) {
             resume(disabled_area);
         } else {
             vmkit_vmenter(dcb);
@@ -117,7 +111,7 @@ void __attribute__ ((noreturn)) dispatch(struct dcb *dcb)
             disp->disabled = true;
         }
 #if defined(__x86_64__) && !defined(__k1om__)
-        if(!dcb->is_vm_guest) {
+        if (!dcb->is_vm_guest) {
             execute(disp->dispatcher_run);
         } else {
             vmkit_vmexec(dcb, (disp) ? disp->dispatcher_run : 0);
@@ -126,7 +120,7 @@ void __attribute__ ((noreturn)) dispatch(struct dcb *dcb)
         execute(disp->dispatcher_run);
 #endif
     }
-} // end function: dispatch
+}  // end function: dispatch
 
 /**
  * \brief Transfer cap from 'send' to 'ep', according to 'msg'.
@@ -142,8 +136,7 @@ void __attribute__ ((noreturn)) dispatch(struct dcb *dcb)
  * \return      Error code
  */
 static errval_t lmp_transfer_cap(struct capability *ep, struct dcb *send,
-                                 capaddr_t send_cptr, uint8_t send_level,
-                                 bool give_away)
+                                 capaddr_t send_cptr, uint8_t send_level, bool give_away)
 {
     errval_t err;
     /* Parameter checking */
@@ -155,31 +148,28 @@ static errval_t lmp_transfer_cap(struct capability *ep, struct dcb *send,
     assert(recv != NULL);
     assert(ep->u.endpointlmp.epoffset != 0);
 
-    // printk(LOG_NOTE, "%s: ep->u.endpointlmp.epoffset = %"PRIuLVADDR"\n", __FUNCTION__, ep->u.endpointlmp.epoffset);
+    // printk(LOG_NOTE, "%s: ep->u.endpointlmp.epoffset = %"PRIuLVADDR"\n", __FUNCTION__,
+    // ep->u.endpointlmp.epoffset);
     /* Look up the slot receiver can receive caps in */
-    struct lmp_endpoint_kern *recv_ep
-        = (void *)((uint8_t *)recv->disp + ep->u.endpointlmp.epoffset);
+    struct lmp_endpoint_kern *recv_ep = (void *)((uint8_t *)recv->disp
+                                                 + ep->u.endpointlmp.epoffset);
 
     // Lookup cspace root for receiving
     struct capability *recv_cspace_cap;
     // XXX: do we want a level into receiver's cspace here?
     // printk(LOG_NOTE, "recv_cspace_ptr = %"PRIxCADDR"\n", recv_ep->recv_cspc);
-    err = caps_lookup_cap(&recv->cspace.cap, recv_ep->recv_cspc, 2,
-                          &recv_cspace_cap, CAPRIGHTS_READ_WRITE);
+    err = caps_lookup_cap(&recv->cspace.cap, recv_ep->recv_cspc, 2, &recv_cspace_cap,
+                          CAPRIGHTS_READ_WRITE);
     if (err_is_fail(err) || recv_cspace_cap->type != ObjType_L1CNode) {
-        printk(LOG_NOTE, "failed a\n");
-        printk(LOG_NOTE, "type %d\n", recv_cspace_cap->type);
         return SYS_ERR_LMP_CAPTRANSFER_DST_CNODE_INVALID;
     }
     // Check index into L1 cnode
     capaddr_t l1index = recv_ep->recv_cptr >> L2_CNODE_BITS;
     if (l1index >= cnode_get_slots(recv_cspace_cap)) {
-        printk(LOG_NOTE, "failed b\n");
         return SYS_ERR_LMP_CAPTRANSFER_DST_CNODE_INVALID;
     }
     // Get the cnode
-    struct cte *recv_cnode_cte = caps_locate_slot(get_address(recv_cspace_cap),
-                                                  l1index);
+    struct cte *recv_cnode_cte = caps_locate_slot(get_address(recv_cspace_cap), l1index);
     struct capability *recv_cnode_cap = &recv_cnode_cte->cap;
     // Check for cnode type
     if (recv_cnode_cap->type != ObjType_L2CNode) {
@@ -193,8 +183,8 @@ static errval_t lmp_transfer_cap(struct capability *ep, struct dcb *send,
 
     /* Look up source slot in sender */
     struct cte *send_cte;
-    err = caps_lookup_slot(&send->cspace.cap, send_cptr, send_level,
-                           &send_cte, CAPRIGHTS_READ);
+    err = caps_lookup_slot(&send->cspace.cap, send_cptr, send_level, &send_cte,
+                           CAPRIGHTS_READ);
     if (err_is_fail(err)) {
         return err_push(err, SYS_ERR_LMP_CAPTRANSFER_SRC_LOOKUP);
     }
@@ -205,20 +195,21 @@ static errval_t lmp_transfer_cap(struct capability *ep, struct dcb *send,
         return SYS_ERR_LMP_CAPTRANSFER_DST_SLOT_OCCUPIED;
     }
 
-    //caps_trace(__func__, __LINE__, send_cte, "transferring");
-    //TRACE_CAP_MSG("transferring", send_cte);
+    // caps_trace(__func__, __LINE__, send_cte, "transferring");
+    // TRACE_CAP_MSG("transferring", send_cte);
 
     /* Insert send cap into recv cap */
     err = caps_copy_to_cte(recv_cte, send_cte, false, 0, 0);
-    assert(err_is_ok(err)); // Cannot fail after checking that slot is empty
+    assert(err_is_ok(err));  // Cannot fail after checking that slot is empty
 
     if (give_away) {
         err = caps_delete(send_cte);
         if (err_is_fail(err)) {
-            printk(LOG_NOTE, "deleting source of lmp captransfer failed: %"PRIuERRV"\n", err);
+            printk(LOG_NOTE, "deleting source of lmp captransfer failed: %" PRIuERRV "\n",
+                   err);
         }
-        assert(err_is_ok(err)); // A copy now exists in the recv slot, so this
-                                // should not fail
+        assert(err_is_ok(err));  // A copy now exists in the recv slot, so this
+                                 // should not fail
     }
 
     return SYS_ERR_OK;
@@ -230,8 +221,7 @@ static errval_t lmp_transfer_cap(struct capability *ep, struct dcb *send,
  * \param ep     Endpoint capability to send to
  * \param payload_len Length (in number of words) of payload
  */
-errval_t lmp_can_deliver_payload(struct capability *ep,
-                                 size_t payload_len)
+errval_t lmp_can_deliver_payload(struct capability *ep, size_t payload_len)
 {
     assert(ep != NULL);
     assert(ep->type == ObjType_EndPointLMP);
@@ -244,8 +234,8 @@ errval_t lmp_can_deliver_payload(struct capability *ep,
     }
 
     /* locate receiver's endpoint buffer */
-    struct lmp_endpoint_kern *recv_ep
-        = (void *)((uint8_t *)recv->disp + ep->u.endpointlmp.epoffset);
+    struct lmp_endpoint_kern *recv_ep = (void *)((uint8_t *)recv->disp
+                                                 + ep->u.endpointlmp.epoffset);
 
     /* check delivered/consumed state */
     uint32_t epbuflen = ep->u.endpointlmp.epbuflen;
@@ -285,9 +275,8 @@ errval_t lmp_can_deliver_payload(struct capability *ep,
  *
  * \return Error code
  */
-errval_t lmp_deliver_payload(struct capability *ep, struct dcb *send,
-                             uintptr_t *payload, size_t payload_len,
-                             bool captransfer, bool now)
+errval_t lmp_deliver_payload(struct capability *ep, struct dcb *send, uintptr_t *payload,
+                             size_t payload_len, bool captransfer, bool now)
 {
     assert(ep != NULL);
     assert(ep->type == ObjType_EndPointLMP);
@@ -303,20 +292,19 @@ errval_t lmp_deliver_payload(struct capability *ep, struct dcb *send,
     }
 
     /* locate receiver's endpoint buffer */
-    struct lmp_endpoint_kern *recv_ep
-        = (void *)((uint8_t *)recv->disp + ep->u.endpointlmp.epoffset);
+    struct lmp_endpoint_kern *recv_ep = (void *)((uint8_t *)recv->disp
+                                                 + ep->u.endpointlmp.epoffset);
 
     /* read current pos and buflen */
     uint32_t epbuflen = ep->u.endpointlmp.epbuflen;
     uint32_t pos = recv_ep->delivered;
 
-    struct dispatcher_shared_generic *send_disp =
-        send ? get_dispatcher_shared_generic(send->disp) : NULL;
-    struct dispatcher_shared_generic *recv_disp =
-        get_dispatcher_shared_generic(recv->disp);
-    debug(SUBSYS_DISPATCH, "LMP %.*s -> %.*s\n",
-          DISP_NAME_LEN, send ? send_disp->name : "kernel",
-          DISP_NAME_LEN, recv_disp->name);
+    struct dispatcher_shared_generic *send_disp
+        = send ? get_dispatcher_shared_generic(send->disp) : NULL;
+    struct dispatcher_shared_generic *recv_disp = get_dispatcher_shared_generic(
+        recv->disp);
+    debug(SUBSYS_DISPATCH, "LMP %.*s -> %.*s\n", DISP_NAME_LEN,
+          send ? send_disp->name : "kernel", DISP_NAME_LEN, recv_disp->name);
 
     // Setup receiver's message flags
     union lmp_recv_header recvheader = { .raw = 0 };
@@ -330,7 +318,7 @@ errval_t lmp_deliver_payload(struct capability *ep, struct dcb *send,
     }
 
     /* Transfer the msg */
-    for(int i = 0; i < payload_len; i++) {
+    for (int i = 0; i < payload_len; i++) {
         recv_ep->buf[pos] = payload[i];
         if (++pos == epbuflen) {
             pos = 0;
@@ -364,9 +352,8 @@ errval_t lmp_deliver_payload(struct capability *ep, struct dcb *send,
  * \param send_cptr Capability to be transferred with LMP
  * \param send_level CSpace level of cptr
  */
-errval_t lmp_deliver(struct capability *ep, struct dcb *send,
-                     uintptr_t *payload, size_t len,
-                     capaddr_t send_cptr, uint8_t send_level, bool give_away)
+errval_t lmp_deliver(struct capability *ep, struct dcb *send, uintptr_t *payload,
+                     size_t len, capaddr_t send_cptr, uint8_t send_level, bool give_away)
 {
     bool captransfer;
     assert(ep != NULL);
