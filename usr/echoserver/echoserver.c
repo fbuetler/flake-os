45508e74682b4986d2669285f7c61e8d73a1a768
Florian Buetler <flbuetle@ethz.ch>
2022-05-11 19:32:12 +0200

    boiler plate a echo server

---
+++ usr/echoserver/echoserver.c
@@ -0,0 +1,30 @@
+/**
+ * \file
+ * \brief Echo server application
+ */
+
+/*
+ * Copyright (c) 2016 ETH Zurich.
+ * All rights reserved.
+ *
+ * This file is distributed under the terms in the attached LICENSE file.
+ * If you do not find this file, copies can be found by writing to:
+ * ETH Zurich D-INFK, CAB F.78, Universitaetstr. 6, CH-8092 Zurich,
+ * Attn: Systems Group.
+ */
+
+
+#include <stdio.h>
+
+#include <aos/aos.h>
+
+int main(int argc, char *argv[])
+{
+    // start server on port
+
+    // listen for messages
+
+    // echo them back
+
+    return EXIT_SUCCESS;
+}
